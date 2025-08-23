655f5d2b0fe77baa5a0548063bf5b60283c7365d
Billal BENYOUB <benyoubbillal@outlook.fr>
2025-08-23 23:51:24 +0200

    add seeder

--- TennisCleanArchi.Host/Program.cs
+++ TennisCleanArchi.Host/Program.cs
@@ -1,8 +1,9 @@
 using TennisCleanArchi.Infrastructure;
+using TennisCleanArchi.Infrastructure.Persistance;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddInfrastructure();
+builder.AddInfrastructure();
 
 builder.Services.AddControllers();
 
@@ -16,5 +17,12 @@ app.UseInfrastructure();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Seed the database
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetRequiredService<ApplicationSeeder>();
+    await seeder.SeedAsync();
+}
 
 app.Run();
--- TennisCleanArchi.Infrastructure/Startup.cs
+++ TennisCleanArchi.Infrastructure/Startup.cs
@@ -1,15 +1,24 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TennisCleanArchi.Infrastructure.OpenApi;
+using TennisCleanArchi.Infrastructure.Persistance;
 
 namespace TennisCleanArchi.Infrastructure;
 
 public static class Startup
 {
-    public static IHostApplicationBuilder AddInfrastructure(this IHostApplicationBuilder applicationBuilder)
+    public static IHostApplicationBuilder AddInfrastructure(this IHostApplicationBuilder builder)
     {
-        applicationBuilder.Services.AddSwagger();
-        return applicationBuilder;
+        builder.Services.AddDbContext<ApplicationDbContext>(
+            options => options.UseInMemoryDatabase("TennisDatabase"));
+
+        builder.Services.AddTransient<ApplicationSeeder>();
+
+        builder.Services.AddSwagger();
+
+        return builder;
     }
 
     public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
