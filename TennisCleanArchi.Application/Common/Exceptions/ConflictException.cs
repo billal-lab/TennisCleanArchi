3b8d3d0727a9f89aec1e35ea836b86d9cf7d2d58
Billal BENYOUB <benyoubbillal@outlook.fr>
2025-08-24 19:41:09 +0200

    feat: add conflict handling for duplicate player ranks and implement ConflictException

---
+++ TennisCleanArchi.Application/Common/Exceptions/ConflictException.cs
@@ -0,0 +1,13 @@
+﻿namespace TennisCleanArchi.Application.Common.Exceptions;
+
+public class ConflictException : CustomException
+{
+    public ConflictException(string message)
+        : base(message, null, System.Net.HttpStatusCode.Conflict)
+    {
+    }
+    public ConflictException(string message, string errorCode)
+      : base(message, null, System.Net.HttpStatusCode.Conflict, errorCode)
+    {
+    }
+}
